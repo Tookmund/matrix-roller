71ceab5a9c0078d54bda36e4100ecdc85e8308dd
Jacob Adams <tookmund@gmail.com>
2017-07-12 17:15:08 -0400

    update

--- dice.js
+++ dice.js
@@ -1,4 +1,20 @@
-var client = matrixcs.createClient("http://matrix.org");
-client.on("event",function(event) {
-	document.body.innerHTML = document.body.innerHTML+event.getType()+"\n";
+function log(str) {
+	document.body.innerHTML = document.body.innerHTML+"\n"+str;
+}
+var client = matrixcs.createClient({
+	baseUrl: "http://matrix.org",
+	accessToken: access,
+	userId: userId
 });
+client.on("RoomMember.membership", function(event, member) {
+	if (member.membership === "invite" && member.userId === myUserId) {
+        	client.joinRoom(member.roomId).done(function() {
+               		log("Auto-joined "+member.roomId);
+           	});
+       }
+});
+client.on("Room.timeline", function(event, room, toStart, removed, data) {
+	if (event.getType() != "m.room.message") return;
+	log(event.getContent().body);	
+});
+client.startClient();
